3dab318e000b0f6e7e1a7e55839525167d3a10ee
sotnii <ruslan.sot70@gmail.com>
2026-05-10 13:17:44 +0300

    doc: getting started & examples pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightThemeRapide from "starlight-theme-rapide";
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    mermaid({
+      theme: "forest",
+      autoTheme: true,
+    }),
     starlight({
       title: "Pakostii",
       social: [
@@ -19,7 +24,7 @@ export default defineConfig({
       sidebar: [
         {
           label: "Start here",
-          items: ["about", "getting-started"],
+          items: ["about", "getting-started", "examples"],
         },
       ],
     }),
